181aa8f45859a22dfc70b381a5c12b981c4c4e38
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-02-24 13:33:18 +0800

    frontend: connectin to apollo client

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -2,19 +2,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import '../components/styles/nprogress.css';
 
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
-);
+const MyApp = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
 
-export default MyApp;
+// if any of the components needs initialProps to start with
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
