012cffb44bb1c661cfe57fa23986b9602d673e1a
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-03-04 00:44:30 +0800

    deleting products

--- sick-fits/frontend/components/Product.js
+++ sick-fits/frontend/components/Product.js
@@ -1,33 +1,55 @@
 import Link from 'next/link';
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import ItemStyles from './styles/ItemStyles';
 import Title from './styles/Title';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 
-const Product = ({ product }) => (
-  <ItemStyles>
-    <img src={product?.photo?.image?.publicUrlTransformed} alt={product.name} />
-    <Title>
-      <Link href={`/product/${product.id}`}>{product.name}</Link>
-    </Title>
-    <PriceTag>{formatMoney(product.price)}</PriceTag>
-    <p>{product.description}</p>
-    {/* TODO: add buttons to do edit and delete item */}
-    <div className="buttonList">
-      <Link
-        href={{
-          pathname: '/update',
-          query: {
-            id: product.id,
-          },
-        }}
-      >
-        Edit 💅
-      </Link>
-    </div>
-  </ItemStyles>
-);
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+const Product = ({ product }) => {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id: product.id },
+  });
+
+  if (loading) return <p>loading...</p>;
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      {/* TODO: add buttons to do edit and delete item */}
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: '/update',
+            query: {
+              id: product.id,
+            },
+          }}
+        >
+          Edit 💅
+        </Link>
+        <button type="button">🧼 delete</button>
+      </div>
+    </ItemStyles>
+  );
+};
 
 Product.propTypes = {
   product: PropTypes.object,
