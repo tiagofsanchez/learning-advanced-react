f2d5d3c251d53de1af2538d331453a48b6d60fc0
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-03-26 21:48:39 +0800

    backend: linking to stripe

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -1,5 +1,9 @@
 import { KeystoneContext } from '@keystone-next/types';
-import { OrderCreateInput } from '../.keystone/schema-types';
+import {
+  CardItemCreateInput,
+  OrderCreateInput,
+} from '../.keystone/schema-types';
+import stripeConfig from '../lib/stripe';
 
 // this is a great way to have syntax highlight
 const graphql = String.raw;
@@ -9,7 +13,6 @@ async function checkout(
   { token }: { token: string },
   context: KeystoneContext
 ): Promise<OrderCreateInput> {
-  console.log('Checking Ouuttt!');
   // 1. Make sure they are signed in
   const userId = context.session.itemId as Session;
   if (!userId) {
@@ -42,10 +45,31 @@ async function checkout(
     `,
   });
 
-  console.dir(user, { depth: null });
+  // console.dir(user, { depth: null });
 
   // 2. Calculate the total price for the order
+  const cartItems = user.cart.filter((cart) => cart.product);
+  const amount = cartItems.reduce(function (
+    tally: number,
+    cartItem: CardItemCreateInput
+  ) {
+    return tally + cartItem.quantity * cartItem.product.price;
+  },
+  0);
+  console.log(amount);
+
   // 3. Create the payment with stripe lib
+  const charge = await stripeConfig.paymentIntents
+    .create({
+      amount,
+      currency: 'USD',
+      confirm: true,
+      payment_method: token,
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err.message);
+    });
   // 4. Convert the cardItems to OrderItems
   // 5. Create the order and return it
 }
