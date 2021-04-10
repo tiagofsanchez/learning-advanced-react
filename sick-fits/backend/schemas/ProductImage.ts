8b272a2e633da48ce1f289671c97af5461e7af6a
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-04-10 21:31:40 +0800

    backend: changed cloudinary folder on the backend

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -7,7 +7,7 @@ export const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_KEY,
   apiSecret: process.env.CLOUDINARY_SECRET,
-  folder: 'sickfits',
+  folder: 'tfsphotos',
 };
 
 // console.log('🚨_______________🚨');
